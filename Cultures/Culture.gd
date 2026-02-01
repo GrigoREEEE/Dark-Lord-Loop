32f13ae3e537c2e0c2ac05018924fd1949278487
GrigoREEEE <58792857+GrigoREEEE@users.noreply.github.com>
2026-02-01 18:40:42 -0500

    Cultures

---
+++ Cultures/Culture.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name culture
+
+var id : String #CU####
+var culture_name : String 
+var culture_value : String
+var nations_in_culture : Array
+
+##### Values #####
+### Sociatal Structure ###
+var social_equality : float # How much privelege the higher social classes have over the lower ones
+var social_mobility : float # How easy it is to move up in social class
+var social
+
+### Thought ###
+var conformism : float # How much free thought is allowed
+var sexuality : float # How sexually liberated the society is
